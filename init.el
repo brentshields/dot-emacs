6ef13e9689074c2640ed4693276835601026845a
Brent Shields <shieldsbrent@gmail.com>
2017-10-24 13:21:32 -0400

    Web mode and purescript mode

--- init.el
+++ init.el
@@ -47,6 +47,8 @@
        (0 (progn (compose-region (match-beginning 1) (match-end 1)
                                  (string-to-char "λ"))))))))
 
+(ensure-installed 'purescript-mode)
+
 (ensure-installed 'markdown-mode)
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
@@ -111,3 +113,21 @@
   (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
 
 (ensure-installed 'jade-mode)
+
+(ensure-installed 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;;(add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "#8DE6F4")
+  (no-whitespace)
+)
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
